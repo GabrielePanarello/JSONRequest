16174aa5d9c8e6a190a7b3ad16fedfa344cadd4f
GabrielePanarello <gbrlpanarello@gmail.com>
2018-03-27 20:34:15 +0200

    Removed unused imports

--- app/src/main/java/com/example/gabrielepanarello/jsonrequest/MainActivity.java
+++ app/src/main/java/com/example/gabrielepanarello/jsonrequest/MainActivity.java
@@ -1,17 +1,12 @@
 package com.example.gabrielepanarello.jsonrequest;
 
 import android.app.ProgressDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.VolleyLog;
-import com.android.volley.toolbox.JsonObjectRequest;
-
-import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,24 +22,6 @@ public class MainActivity extends AppCompatActivity {
         final ProgressDialog dialog = new ProgressDialog(MainActivity.this);
         dialog.setMessage("Loading..");
         dialog.show();
-
-        /*JsonObjectRequest jsonObjectReq = new JsonObjectRequest("https://androidtutorialpoint.com/api/volleyJsonObject", null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        output.setText(response.toString());
-                        Log.d("SERVICE", "Response: " + response.toString());
-                        dialog.hide();
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                VolleyLog.d("VOLLEY", "Error: " + error.getMessage());
-                dialog.hide();
-            }
-        });*/
-        // Access the RequestQueue through your singleton class.
-
 
         GsonRequest jsonObjectReq = new GsonRequest("https://androidtutorialpoint.com/api/volleyJsonObject", DeviceOutput.class, null,
                 new Response.Listener<DeviceOutput>() {
